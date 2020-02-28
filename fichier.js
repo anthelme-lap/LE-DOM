5ee5d3ead51242f7eb7a9917b311602e7ac4cec1
anthelme-lap <anthelmekoffi77@gmail.com>
2020-02-28 12:04:24 +0000

    ajoust

--- fichier.js
+++ fichier.js
@@ -1,3 +1,14 @@
 document.querySelector('#para').innerHTML="salut! Moi c'est anthelme et c'est ma premier page javascript soyez indulgent".toLocaleUpperCase()
-document.querySelector('#para').style.color="blue";
-document.getElementById('bjr').innerHTML = document.getElementsByTagName('ul')[0].getElementsByTagName('li')[3]. innerHTML
+document.querySelector('#para').style.color="blue"; // mettre le texte en couleur bleue
+// document.getElementById('bjr').innerHTML = document.getElementsByTagName('ul')[0]. innerHTML // afficher le menu dans le paragraphe
+
+function colorRouge()
+{
+    document.querySelector('#parain').style.color="red";
+}
+
+
+function colorBleu()
+{
+    document.querySelector('#anthelme').style.color="bleu";
+}
