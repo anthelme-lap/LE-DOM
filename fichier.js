4d66a49d96a70f02c14b469274aadf5b77a4eb12
anthelme-lap <anthelmekoffi77@gmail.com>
2020-02-28 11:11:48 +0000

    fichier js

--- fichier.js
+++ fichier.js
@@ -1,3 +1,3 @@
 document.querySelector('#para').innerHTML="salut! Moi c'est anthelme et c'est ma premier page javascript soyez indulgent".toLocaleUpperCase()
 document.querySelector('#para').style.color="blue";
-document.getElementsByTagNameNS
+document.getElementsByTagName("ul").[0].innerHTML=""
